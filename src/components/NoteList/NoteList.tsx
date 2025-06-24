57bea39c517e5d064cce65c1be1facd4d5dc4c28
VyshnevskaO <vyshnevskaolenka@gmail.com>
2025-06-24 23:13:21 +0200

    second commit

--- src/components/NoteList/NoteList.tsx
+++ src/components/NoteList/NoteList.tsx
@@ -1,5 +1,5 @@
 import css from "./NoteList.module.css";
-import type { Note } from "../../types/note";
+import type { Note } from "../../../types/note";
 import { deleteNote } from "../../services/noteService";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "react-toastify";
